4470a73139189424761a12e2326e8fdc9b4cd5f8
Kevin Herron <kevinherron@gmail.com>
2016-06-15 10:10:58 -0700

    Fix handling of null policyId in AnonymousProvider

---
+++ opc-ua-sdk/sdk-tests/src/test/java/org/eclipse/milo/opcua/sdk/client/api/identity/AnonymousProviderTest.java
@@ -0,0 +1,104 @@
+package org.eclipse.milo.opcua.sdk.client.api.identity;
+
+import org.eclipse.milo.opcua.stack.core.types.builtin.ByteString;
+import org.eclipse.milo.opcua.stack.core.types.enumerated.UserTokenType;
+import org.eclipse.milo.opcua.stack.core.types.structured.AnonymousIdentityToken;
+import org.eclipse.milo.opcua.stack.core.types.structured.EndpointDescription;
+import org.eclipse.milo.opcua.stack.core.types.structured.SignatureData;
+import org.eclipse.milo.opcua.stack.core.types.structured.UserIdentityToken;
+import org.eclipse.milo.opcua.stack.core.types.structured.UserTokenPolicy;
+import org.jooq.lambda.tuple.Tuple2;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertThrows;
+import static org.testng.Assert.assertTrue;
+
+public class AnonymousProviderTest {
+
+    @Test
+    public void testGetIdentityToken() throws Exception {
+        EndpointDescription endpoint = new EndpointDescription(
+            null, null, null, null, null,
+            new UserTokenPolicy[]{
+                new UserTokenPolicy(
+                    "anonymous",
+                    UserTokenType.Anonymous,
+                    null, null, null)
+            },
+            null, null
+        );
+
+        AnonymousProvider p = new AnonymousProvider();
+
+        Tuple2<UserIdentityToken, SignatureData> t2 =
+            p.getIdentityToken(endpoint, ByteString.NULL_VALUE);
+
+        assertEquals(t2.v1().getPolicyId(), "anonymous");
+        assertTrue(t2.v1() instanceof AnonymousIdentityToken);
+    }
+
+    @Test
+    public void testGetIdentityToken_EmptyPolicyId() throws Exception {
+        EndpointDescription endpoint = new EndpointDescription(
+            null, null, null, null, null,
+            new UserTokenPolicy[]{
+                new UserTokenPolicy(
+                    "",
+                    UserTokenType.Anonymous,
+                    null, null, null)
+            },
+            null, null
+        );
+
+        AnonymousProvider p = new AnonymousProvider();
+
+        Tuple2<UserIdentityToken, SignatureData> t2 =
+            p.getIdentityToken(endpoint, ByteString.NULL_VALUE);
+
+        assertEquals(t2.v1().getPolicyId(), "");
+        assertTrue(t2.v1() instanceof AnonymousIdentityToken);
+    }
+
+    @Test
+    public void testGetIdentityToken_NullPolicyId() throws Exception {
+        EndpointDescription endpoint = new EndpointDescription(
+            null, null, null, null, null,
+            new UserTokenPolicy[]{
+                new UserTokenPolicy(
+                    null,
+                    UserTokenType.Anonymous,
+                    null, null, null)
+            },
+            null, null
+        );
+
+        AnonymousProvider p = new AnonymousProvider();
+
+        Tuple2<UserIdentityToken, SignatureData> t2 =
+            p.getIdentityToken(endpoint, ByteString.NULL_VALUE);
+
+        assertNull(t2.v1().getPolicyId());
+        assertTrue(t2.v1() instanceof AnonymousIdentityToken);
+    }
+
+    @Test
+    public void testGetIdentityToken_NoMatch_Throws() {
+        EndpointDescription endpoint = new EndpointDescription(
+            null, null, null, null, null,
+            new UserTokenPolicy[]{
+                new UserTokenPolicy(
+                    "username",
+                    UserTokenType.UserName,
+                    null, null, null)
+            },
+            null, null
+        );
+
+        AnonymousProvider p = new AnonymousProvider();
+
+        assertThrows(() -> p.getIdentityToken(endpoint, ByteString.NULL_VALUE));
+    }
+
+}
