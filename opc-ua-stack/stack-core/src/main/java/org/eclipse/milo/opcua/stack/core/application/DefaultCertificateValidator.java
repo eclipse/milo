354e9517f527594dd83fcb3f85005a29a1666a3c
Jens Reimann <jreimann@redhat.com>
2016-07-21 13:48:32 +0200

    [#26] DefaultCertificateValidator may leak resources
    This change allows one to properly close the validator, cleaning up the
    internal thread and file system watcher.
    
    It also adds a new constructor which allows to construct the instance
    providing all necessary directories and allows one to disable writing
    the rejected certificates.
    
    A test case was also added to test the shutdown.
    
    Signed-off-by: Jens Reimann <jreimann@redhat.com>

--- opc-ua-stack/stack-core/src/main/java/org/eclipse/milo/opcua/stack/core/application/DefaultCertificateValidator.java
+++ opc-ua-stack/stack-core/src/main/java/org/eclipse/milo/opcua/stack/core/application/DefaultCertificateValidator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Kevin Herron
+ * Copyright (c) 2016 Kevin Herron and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -19,7 +19,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URLEncoder;
-import java.nio.file.FileSystems;
+import java.nio.file.ClosedWatchServiceException;
 import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
@@ -28,10 +28,14 @@ import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
+import com.google.common.util.concurrent.Uninterruptibles;
 import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
 import org.eclipse.milo.opcua.stack.core.UaException;
@@ -43,7 +47,7 @@ import org.slf4j.LoggerFactory;
 
 import static java.util.stream.Collectors.toSet;
 
-public class DefaultCertificateValidator implements CertificateValidator {
+public class DefaultCertificateValidator implements CertificateValidator, AutoCloseable {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -53,6 +57,9 @@ public class DefaultCertificateValidator implements CertificateValidator {
     private final File trustedDir;
     private final File rejectedDir;
     private final File revocationDir;
+
+    private WatchService watchService;
+    private Thread thread;
 
     public DefaultCertificateValidator(File certificatesBaseDir) {
         trustedDir = new File(certificatesBaseDir.getAbsolutePath() + File.separator + "trusted");
@@ -75,11 +82,98 @@ public class DefaultCertificateValidator implements CertificateValidator {
         synchronizeTrustedCertificates();
     }
 
+    /**
+     * Create a new instance specifying explicit directories
+     * <br/>
+     * If the parameter {@code rejectedDir} is {@code null}, then the validator will not write out
+     * the rejected certificates
+     * <br/>
+     * In contrast to the constructor {@link #DefaultCertificateValidator(File)}, this constructor
+     * expects all directories to be already existing.
+     *
+     * @param trustedDir    the directory of trusted certificates
+     * @param rejectedDir   the optional directory of rejected certificates, may be {@code null}
+     * @param revocationDir the optional directory of revoked certificates, may be {@code null}
+     */
+    public DefaultCertificateValidator(
+        @Nonnull File trustedDir,
+        @Nullable File rejectedDir,
+        @Nullable File revocationDir) {
+
+        Objects.requireNonNull(trustedDir);
+
+        this.trustedDir = trustedDir.getAbsoluteFile();
+        this.rejectedDir = rejectedDir != null ? rejectedDir.getAbsoluteFile() : null;
+        this.revocationDir = revocationDir != null ? revocationDir.getAbsoluteFile() : null;
+
+        if (!trustedDir.isDirectory()) {
+            throw new IllegalArgumentException(
+                String.format("Directory of trusted certificates could not be found: %s", trustedDir.getAbsolutePath())
+            );
+        }
+
+        if (rejectedDir != null && !rejectedDir.isDirectory()) {
+            throw new IllegalArgumentException(
+                String.format(
+                    "Directory of rejected certificates must be an existing, writable directory: %s",
+                    rejectedDir.getAbsolutePath()
+                )
+            );
+        }
+
+        if (revocationDir != null && !revocationDir.isDirectory()) {
+            throw new IllegalArgumentException(
+                String.format(
+                    "Directory of revoked certificates must be an existing, writable directory: %s",
+                    revocationDir.getAbsolutePath()
+                )
+            );
+        }
+
+        createWatchService();
+        synchronizeTrustedCertificates();
+    }
+
+    /**
+     * This will stop the validator and free all resources.
+     * <br/>
+     * After calling this method the method
+     * {@link #verifyTrustChain(X509Certificate, List)} will report all
+     * certificates as invalid.
+     */
+    @Override
+    public void close() throws IOException {
+        final WatchService watchService;
+        final Thread thread;
+
+        // get and reset
+        synchronized (this) {
+            logger.info("Closing default certificate validator");
+
+            watchService = this.watchService;
+            thread = this.thread;
+
+            this.watchService = null;
+            this.thread = null;
+
+            this.trustedCertificates.clear();
+            this.authorityCertificates.clear();
+        }
+
+        // dispose
+        if (watchService != null) {
+            watchService.close();
+        }
+
+        if (thread != null) {
+            Uninterruptibles.joinUninterruptibly(thread);
+        }
+    }
+
     @Override
     public void validate(X509Certificate certificate) throws UaException {
         try {
             CertificateValidationUtil.validateCertificateValidity(certificate);
-
         } catch (UaException e) {
             certificateRejected(certificate);
             throw e;
@@ -92,7 +186,6 @@ public class DefaultCertificateValidator implements CertificateValidator {
         try {
             CertificateValidationUtil.validateTrustChain(
                 certificate, chain, trustedCertificates, authorityCertificates);
-
         } catch (UaException e) {
             certificateRejected(certificate);
             throw e;
@@ -115,7 +208,7 @@ public class DefaultCertificateValidator implements CertificateValidator {
 
     private void createWatchService() {
         try {
-            WatchService watchService = FileSystems.getDefault().newWatchService();
+            this.watchService = trustedDir.toPath().getFileSystem().newWatchService();
 
             WatchKey trustedKey = trustedDir.toPath().register(
                 watchService,
@@ -124,10 +217,10 @@ public class DefaultCertificateValidator implements CertificateValidator {
                 StandardWatchEventKinds.ENTRY_MODIFY
             );
 
-            Thread thread = new Thread(new Watcher(watchService, trustedKey));
-            thread.setName("ua-certificate-directory-watcher");
-            thread.setDaemon(true);
-            thread.start();
+            this.thread = new Thread(new Watcher(watchService, trustedKey));
+            this.thread.setName("ua-certificate-directory-watcher");
+            this.thread.setDaemon(true);
+            this.thread.start();
         } catch (IOException e) {
             logger.error("Error creating WatchService.", e);
         }
@@ -180,6 +273,10 @@ public class DefaultCertificateValidator implements CertificateValidator {
     }
 
     private void certificateRejected(X509Certificate certificate) {
+        if (rejectedDir == null) {
+            return;
+        }
+
         try {
             String[] ss = certificate.getSubjectX500Principal().getName().split(",");
             String name = ss.length > 0 ? ss[0] : certificate.getSubjectX500Principal().getName();
@@ -189,10 +286,10 @@ public class DefaultCertificateValidator implements CertificateValidator {
 
             File f = new File(rejectedDir.getAbsolutePath() + File.separator + filename);
 
-            FileOutputStream fos = new FileOutputStream(f);
-            fos.write(certificate.getEncoded());
-            fos.flush();
-            fos.close();
+            try (FileOutputStream fos = new FileOutputStream(f)) {
+                fos.write(certificate.getEncoded());
+                fos.flush();
+            }
 
             logger.debug("Added rejected certificate entry: {}", filename);
         } catch (CertificateEncodingException | IOException e) {
@@ -205,7 +302,7 @@ public class DefaultCertificateValidator implements CertificateValidator {
         private final WatchService watchService;
         private final WatchKey trustedKey;
 
-        public Watcher(WatchService watchService, WatchKey trustedKey) {
+        Watcher(WatchService watchService, WatchKey trustedKey) {
             this.watchService = watchService;
             this.trustedKey = trustedKey;
         }
@@ -227,8 +324,12 @@ public class DefaultCertificateValidator implements CertificateValidator {
                     }
 
                     if (!key.reset()) {
+                        logger.warn("Failed to reset watch key");
                         break;
                     }
+                } catch (ClosedWatchServiceException e) {
+                    logger.info("Watcher got closed");
+                    return;
                 } catch (InterruptedException e) {
                     logger.error("Watcher interrupted.", e);
                 }
---
+++ opc-ua-stack/stack-core/src/test/java/org/eclipse/milo/opcua/stack/core/application/DefaultCertificateValidatorTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2016 Jens Reimann <jreimann@redhat.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *  http://www.eclipse.org/org/documents/edl-v10.html.
+ */
+
+package org.eclipse.milo.opcua.stack.core.application;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Comparator;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DefaultCertificateValidatorTest {
+
+    private Path basePath = Paths.get("certs.test1");
+
+    @Test(timeOut = 5_000)
+    public void testLifecycleClose() throws IOException {
+        int initialThreadCount = countThreads("init");
+        try (DefaultCertificateValidator validator = new DefaultCertificateValidator(this.basePath.toFile())) {
+            // no-op
+            Assert.assertTrue(countThreads("opened") > initialThreadCount);
+        }
+        Assert.assertEquals(countThreads("closed"), initialThreadCount);
+    }
+
+    private int countThreads(String label) {
+        System.out.format("========================= %s =========================%n", label);
+        Thread[] threads = Thread.getAllStackTraces().keySet().toArray(new Thread[0]);
+        Arrays.sort(threads, Comparator.comparing(Thread::getName));
+
+        for (Thread thread : threads) {
+            System.out.println(thread);
+        }
+        System.out.format("========================= %s =========================%n", label);
+        return threads.length;
+    }
+    
+}
