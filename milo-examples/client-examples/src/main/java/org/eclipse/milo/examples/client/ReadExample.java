8a8bade6a58413d748cb71c355ea4f5fef74d126
Kevin Herron <kevinherron@gmail.com>
2016-08-29 20:55:31 -0700

    Add read and write examples.

---
+++ milo-examples/client-examples/src/main/java/org/eclipse/milo/examples/client/ReadExample.java
@@ -0,0 +1,58 @@
+package org.eclipse.milo.examples.client;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import com.google.common.collect.ImmutableList;
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.sdk.client.api.nodes.attached.UaVariableNode;
+import org.eclipse.milo.opcua.stack.core.Identifiers;
+import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
+import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
+import org.eclipse.milo.opcua.stack.core.types.enumerated.ServerState;
+import org.eclipse.milo.opcua.stack.core.types.enumerated.TimestampsToReturn;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReadExample implements ClientExample {
+
+    public static void main(String[] args) throws Exception {
+        ReadExample example = new ReadExample();
+
+        new ClientExampleRunner(example).run();
+    }
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void run(OpcUaClient client, CompletableFuture<OpcUaClient> future) throws Exception {
+        // synchronous connect
+        client.connect().get();
+
+        // synchronous read request via UaVariableNode
+        UaVariableNode node = client.getAddressSpace().getVariableNode(Identifiers.Server_ServerStatus_StartTime);
+        DataValue value = node.readValue().get();
+
+        logger.info("StartTime={}", value.getValue().getValue());
+
+        // asynchronous read request
+        readServerStateAndTime(client).thenAccept(values -> {
+            DataValue v0 = values.get(0);
+            DataValue v1 = values.get(1);
+
+            logger.info("State={}", ServerState.from((Integer) v0.getValue().getValue()));
+            logger.info("CurrentTime={}", v1.getValue().getValue());
+
+            future.complete(client);
+        });
+    }
+
+    private CompletableFuture<List<DataValue>> readServerStateAndTime(OpcUaClient client) {
+        List<NodeId> nodeIds = ImmutableList.of(
+            Identifiers.Server_ServerStatus_State,
+            Identifiers.Server_ServerStatus_CurrentTime);
+
+        return client.readValues(0.0, TimestampsToReturn.Both, nodeIds);
+    }
+
+}
---
+++ milo-examples/client-examples/src/main/java/org/eclipse/milo/examples/client/WriteExample.java
@@ -0,0 +1,54 @@
+package org.eclipse.milo.examples.client;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import com.google.common.collect.ImmutableList;
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
+import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
+import org.eclipse.milo.opcua.stack.core.types.builtin.StatusCode;
+import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WriteExample implements ClientExample {
+
+    public static void main(String[] args) throws Exception {
+        WriteExample example = new WriteExample();
+
+        new ClientExampleRunner(example).run();
+    }
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void run(OpcUaClient client, CompletableFuture<OpcUaClient> future) throws Exception {
+        // synchronous connect
+        client.connect().get();
+
+        List<NodeId> nodeIds = ImmutableList.of(new NodeId(2, "HelloWorld/Int32"));
+
+        for (int i = 0; i < 10; i++) {
+            Variant v = new Variant(i);
+
+            // don't write status or timestamps
+            DataValue dv = new DataValue(v, null, null);
+
+            // write asynchronously....
+            CompletableFuture<List<StatusCode>> f =
+                client.writeValues(nodeIds, ImmutableList.of(dv));
+
+            // ...but block for the results so we write in order
+            List<StatusCode> statusCodes = f.get();
+            StatusCode status = statusCodes.get(0);
+
+            if (status.isGood()) {
+                logger.info("Wrote '{}' to nodeId={}", v, nodeIds.get(0));
+            }
+        }
+
+        future.complete(client);
+    }
+
+}
