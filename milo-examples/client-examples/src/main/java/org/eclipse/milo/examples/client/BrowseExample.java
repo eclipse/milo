264dee11fe552dc527b312c68f8d48185c9a95c0
Ari Suutari <ari.suutari@syncrontech.com>
2016-09-15 11:07:46 +0300

    Add simple address space browsing example
    Signed-off-by: Ari Suutari <ari.suutari@syncrontech.com>

---
+++ milo-examples/client-examples/src/main/java/org/eclipse/milo/examples/client/BrowseExample.java
@@ -0,0 +1,66 @@
+package org.eclipse.milo.examples.client;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.stack.core.Identifiers;
+import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
+import org.eclipse.milo.opcua.stack.core.types.enumerated.BrowseDirection;
+import org.eclipse.milo.opcua.stack.core.types.enumerated.BrowseResultMask;
+import org.eclipse.milo.opcua.stack.core.types.enumerated.NodeClass;
+import org.eclipse.milo.opcua.stack.core.types.structured.BrowseDescription;
+import org.eclipse.milo.opcua.stack.core.types.structured.BrowseResult;
+import org.eclipse.milo.opcua.stack.core.types.structured.ReferenceDescription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.uint;
+
+public class BrowseExample implements ClientExample {
+
+    public static void main(String[] args) throws Exception {
+        BrowseExample example = new BrowseExample();
+
+        new ClientExampleRunner(example).run();
+    }
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void run(OpcUaClient client, CompletableFuture<OpcUaClient> future) throws Exception {
+        // synchronous connect
+        client.connect().get();
+
+        // start browsing at root folder
+        browseNode("", client, Identifiers.RootFolder);
+
+        future.complete(client);
+    }
+
+    private void browseNode(String indent, OpcUaClient client, NodeId browseRoot) {
+        BrowseDescription browse = new BrowseDescription(
+            browseRoot,
+            BrowseDirection.Forward,
+            Identifiers.References,
+            true,
+            uint(NodeClass.Object.getValue() | NodeClass.Variable.getValue()),
+            uint(BrowseResultMask.All.getValue())
+        );
+
+        try {
+            BrowseResult browseResult = client.browse(browse).get();
+
+            for (ReferenceDescription rd : browseResult.getReferences()) {
+                logger.info("{} Node={}", indent, rd.getBrowseName().getName());
+
+                // recursively browse to children
+                rd.getNodeId().local().ifPresent(nodeId -> browseNode(indent + "  ", client, nodeId));
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            logger.error("Browsing nodeId={} failed: {}", browseRoot, e.getMessage(), e);
+        }
+    }
+
+}
+
