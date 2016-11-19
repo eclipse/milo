eaa5b1491e199d3d90962d5a62057989e4156179
Kevin Herron <kevinherron@gmail.com>
2016-11-19 15:55:49 +0100

    Copyright and checkstyle

--- opc-ua-sdk/sdk-server/src/main/java/org/eclipse/milo/opcua/sdk/server/nodes/delegates/GetSetVariableTypeNode.java
+++ opc-ua-sdk/sdk-server/src/main/java/org/eclipse/milo/opcua/sdk/server/nodes/delegates/GetSetVariableTypeNode.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2016 Kevin Herron
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *   http://www.eclipse.org/org/documents/edl-v10.html.
+ */
+
 package org.eclipse.milo.opcua.sdk.server.nodes.delegates;
 
 import java.util.Optional;
@@ -105,7 +118,9 @@ public interface GetSetVariableTypeNode extends GetSetBase {
         node.setValueRank(valueRank);
     }
 
-    default void setArrayDimensions(AttributeContext context, VariableTypeNode node, UInteger[] arrayDimensions) throws UaException {
+    default void setArrayDimensions(
+        AttributeContext context, VariableTypeNode node, UInteger[] arrayDimensions) throws UaException {
+
         node.setArrayDimensions(Optional.ofNullable(arrayDimensions));
     }
 
