2189ad911c9432811d09d1f8be95dd0b7b8f8c52
Kevin Herron <kevinherron@gmail.com>
2016-11-28 13:00:42 -0800

    Add missing copyright headers

--- milo-examples/server-examples/src/main/java/org/eclipse/milo/examples/server/ExampleNamespace.java
+++ milo-examples/server-examples/src/main/java/org/eclipse/milo/examples/server/ExampleNamespace.java
@@ -1,14 +1,14 @@
 /*
- * Copyright (c) 2016 Red Hat
+ * Copyright (c) 2016 Kevin Herron
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Eclipse Distribution License v1.0 which accompany this distribution.
  *
  * The Eclipse Public License is available at
- *    http://www.eclipse.org/legal/epl-v10.html
+ *   http://www.eclipse.org/legal/epl-v10.html
  * and the Eclipse Distribution License is available at
- *    http://www.eclipse.org/org/documents/edl-v10.html.
+ *   http://www.eclipse.org/org/documents/edl-v10.html.
  */
 
 package org.eclipse.milo.examples.server;
--- opc-ua-sdk/sdk-server/src/test/java/org/eclipse/milo/opcua/sdk/server/util/AttributeWriterTest.java
+++ opc-ua-sdk/sdk-server/src/test/java/org/eclipse/milo/opcua/sdk/server/util/AttributeWriterTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Jens Reimann
+ * Copyright (c) 2016 Jens Reimann and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
