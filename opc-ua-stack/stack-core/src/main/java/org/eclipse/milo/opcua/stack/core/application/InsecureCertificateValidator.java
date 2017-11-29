82b75b2381a1b0250822b3ec6868152d4044918e
Kevin Herron <kevinherron@gmail.com>
2017-11-28 16:04:48 -0800

    fixup: use EPLv1 copyright header

--- opc-ua-stack/stack-core/src/main/java/org/eclipse/milo/opcua/stack/core/application/InsecureCertificateValidator.java
+++ opc-ua-stack/stack-core/src/main/java/org/eclipse/milo/opcua/stack/core/application/InsecureCertificateValidator.java
@@ -1,11 +1,14 @@
 /*
  * Copyright (c) 2017 Kevin Herron
  *
- * This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License 2.0
- * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
  *
- * SPDX-License-Identifier: EPL-2.0
+ * The Eclipse Public License is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *   http://www.eclipse.org/org/documents/edl-v10.html.
  */
 
 package org.eclipse.milo.opcua.stack.core.application;
