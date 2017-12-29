4b503ff365c1f33ab3818bad8c14108b8504f5f2
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2017-12-29 09:36:03 +0100

    Add logging for standalone server and remove exception from methods

--- milo-examples/server-examples/src/main/java/org/eclipse/milo/examples/server/SecureServerStandalone.java
+++ milo-examples/server-examples/src/main/java/org/eclipse/milo/examples/server/SecureServerStandalone.java
@@ -24,11 +24,13 @@ import org.eclipse.milo.opcua.stack.core.types.builtin.DateTime;
 import org.eclipse.milo.opcua.stack.core.types.builtin.LocalizedText;
 import org.eclipse.milo.opcua.stack.core.types.structured.*;
 import org.eclipse.milo.opcua.stack.core.util.CryptoRestrictions;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.EnumSet;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static org.eclipse.milo.opcua.sdk.server.api.config.OpcUaServerConfig.*;
@@ -37,24 +39,52 @@ public class SecureServerStandalone {
 
     String ip = System.getProperty("OPCUA_SERVER_IP");
 
+    private static final Logger logger = LoggerFactory.getLogger(SecureServerStandalone.class);
+
     private final OpcUaServer server;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         SecureServerStandalone server = new SecureServerStandalone();
 
-        server.startup().get();
+        try{
+            server.startup().get();
+        }
+        catch (InterruptedException i){
+            logger.error("Starting up the server was interrupted.", i);
+            System.exit(1);
+        }
+        catch (ExecutionException e){
+            logger.error("Concurrency problems encountered during startup", e);
+        }
+
+
 
         final CompletableFuture<Void> future = new CompletableFuture<>();
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> future.complete(null)));
 
-        future.get();
+        try{
+            future.get();
+        }
+        catch (InterruptedException i){
+            logger.error("Running the server was interrupted.", i);
+            System.exit(1);
+        }
+        catch (ExecutionException e){
+            logger.error("Concurrency problems encountered", e);
+        }
     }
 
-    public SecureServerStandalone() throws Exception {
+    public SecureServerStandalone(){
         CryptoRestrictions.remove();
 
-        KeyStoreLoader loader = new KeyStoreLoader().load();
+        KeyStoreLoader loader= new KeyStoreLoader();
+        try {
+            loader = loader.load();
+        }
+        catch (Exception e){
+            logger.error("Could not aquire KeyStoreLoader.", e);
+        }
 
         DefaultCertificateManager certificateManager = new DefaultCertificateManager(
             loader.getServerKeyPair(),
@@ -63,8 +93,7 @@ public class SecureServerStandalone {
 
         File securityTempDir = new File(System.getProperty("java.io.tmpdir"), "security");
 
-        LoggerFactory.getLogger(getClass())
-            .info("security temp dir: {}", securityTempDir.getAbsolutePath());
+        logger.info("security temp dir: {}", securityTempDir.getAbsolutePath());
 
         DefaultCertificateValidator certificateValidator = new DefaultCertificateValidator(securityTempDir);
 
