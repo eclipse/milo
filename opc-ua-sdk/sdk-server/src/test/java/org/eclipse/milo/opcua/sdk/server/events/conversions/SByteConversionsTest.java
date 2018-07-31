d9c44abca2079d498766a1e00a958774e8faee82
Kevin Herron <kevinherron@gmail.com>
2018-07-31 08:00:43 -0700

    SByteConversionsTest

--- opc-ua-sdk/sdk-server/src/test/java/org/eclipse/milo/opcua/sdk/server/events/conversions/SByteConversionsTest.java
+++ opc-ua-sdk/sdk-server/src/test/java/org/eclipse/milo/opcua/sdk/server/events/conversions/SByteConversionsTest.java
@@ -13,65 +13,136 @@
 
 package org.eclipse.milo.opcua.sdk.server.events.conversions;
 
-import org.testng.annotations.Test;
+import org.eclipse.milo.opcua.stack.core.BuiltinDataType;
 
-import static org.testng.Assert.fail;
+import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.ubyte;
+import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.uint;
+import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.ulong;
+import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.ushort;
 
-public class SByteConversionsTest {
+public class SByteConversionsTest extends AbstractConversionTest<Byte> {
 
-    @Test
-    public void testSByteToBoolean() {
-        fail("not implemented");
+    @Override
+    protected Class<Byte> getSourceClass() {
+        return Byte.class;
     }
 
-    @Test
-    public void testSByteToByte() {
-        fail("not implemented");
+    @Override
+    public Conversion[] getConversions(BuiltinDataType targetType) {
+        switch (targetType) {
+            case Boolean:
+                return new Conversion[]{
+                    c((byte) 0, Boolean.FALSE),
+                    c((byte) 1, Boolean.TRUE)
+                };
+
+            case Byte:
+                return new Conversion[]{
+                    c((byte) 0, ubyte(0)),
+                    c(Byte.MAX_VALUE, ubyte(Byte.MAX_VALUE)),
+                    c((byte) -1, null, BuiltinDataType.Byte)
+                };
+
+            case Double: {
+                return new Conversion[]{
+                    c((byte) 0, 0.0),
+                    c(Byte.MIN_VALUE, (double) Byte.MIN_VALUE),
+                    c(Byte.MAX_VALUE, (double) Byte.MAX_VALUE)
+                };
+            }
+
+            case Float: {
+                return new Conversion[]{
+                    c((byte) 0, 0f),
+                    c(Byte.MIN_VALUE, (float) Byte.MIN_VALUE),
+                    c(Byte.MAX_VALUE, (float) Byte.MAX_VALUE)
+                };
+            }
+
+            case Int16: {
+                return new Conversion[]{
+                    c((byte) 0, (short) 0),
+                    c(Byte.MIN_VALUE, (short) Byte.MIN_VALUE),
+                    c(Byte.MAX_VALUE, (short) Byte.MAX_VALUE)
+                };
+            }
+
+            case Int32: {
+                return new Conversion[]{
+                    c((byte) 0, 0),
+                    c(Byte.MIN_VALUE, (int) Byte.MIN_VALUE),
+                    c(Byte.MAX_VALUE, (int) Byte.MAX_VALUE)
+                };
+            }
+
+            case Int64: {
+                return new Conversion[]{
+                    c((byte) 0, 0L),
+                    c(Byte.MIN_VALUE, (long) Byte.MIN_VALUE),
+                    c(Byte.MAX_VALUE, (long) Byte.MAX_VALUE)
+                };
+            }
+
+            case String: {
+                return new Conversion[]{
+                    c((byte) 0, "0"),
+                    c(Byte.MIN_VALUE, Byte.toString(Byte.MIN_VALUE)),
+                    c(Byte.MAX_VALUE, Byte.toString(Byte.MAX_VALUE))
+                };
+            }
+
+            case UInt16: {
+                return new Conversion[]{
+                    c((byte) 0, ushort(0)),
+                    c(Byte.MAX_VALUE, ushort(Byte.MAX_VALUE)),
+                    c((byte) -1, null, targetType)
+                };
+            }
+
+            case UInt32: {
+                return new Conversion[]{
+                    c((byte) 0, uint(0)),
+                    c(Byte.MAX_VALUE, uint(Byte.MAX_VALUE)),
+                    c((byte) -1, null, targetType)
+                };
+            }
+
+            case UInt64: {
+                return new Conversion[]{
+                    c((byte) 0, ulong(0)),
+                    c(Byte.MAX_VALUE, ulong(Byte.MAX_VALUE)),
+                    c((byte) -1, null, targetType)
+                };
+            }
+
+            default:
+                return new Conversion[0];
+        }
     }
 
-    @Test
-    public void testSByteToDouble() {
-        fail("not implemented");
+    @Override
+    public ConversionType getConversionType(BuiltinDataType targetType) {
+        //@formatter:off
+        switch (targetType) {
+            case Boolean:   return ConversionType.EXPLICIT;
+            case Byte:      return ConversionType.EXPLICIT;
+            case Double:    return ConversionType.IMPLICIT;
+            case Float:     return ConversionType.IMPLICIT;
+            case Int16:     return ConversionType.IMPLICIT;
+            case Int32:     return ConversionType.IMPLICIT;
+            case Int64:     return ConversionType.IMPLICIT;
+            case String:    return ConversionType.EXPLICIT;
+            case UInt16:    return ConversionType.IMPLICIT;
+            case UInt32:    return ConversionType.IMPLICIT;
+            case UInt64:    return ConversionType.IMPLICIT;
+            default:        return ConversionType.NONE;
+        }
+        //@formatter:on
     }
 
-    @Test
-    public void testSByteToFloat() {
-        fail("not implemented");
-    }
-
-    @Test
-    public void testSByteToInt16() {
-        fail("not implemented");
-    }
-
-    @Test
-    public void testSByteToInt32() {
-        fail("not implemented");
-    }
-
-    @Test
-    public void testSByteToInt64() {
-        fail("not implemented");
-    }
-
-    @Test
-    public void testSByteToString() {
-        fail("not implemented");
-    }
-
-    @Test
-    public void testSByteToUInt16() {
-        fail("not implemented");
-    }
-
-    @Test
-    public void testSByteToUInt32() {
-        fail("not implemented");
-    }
-
-    @Test
-    public void testSByteToUInt64() {
-        fail("not implemented");
+    @Override
+    protected Object convert(Object fromValue, BuiltinDataType targetType, boolean implicit) {
+        return SByteConversions.convert(fromValue, targetType, implicit);
     }
 
 }
