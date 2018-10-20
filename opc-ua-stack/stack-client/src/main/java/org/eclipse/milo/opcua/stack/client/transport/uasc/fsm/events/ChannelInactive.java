21ff8863b2993dacba3690b692cbf2f5061053d0
Kevin Herron <kevinherron@gmail.com>
2018-10-20 11:16:12 +0200

    formatting fixup

--- opc-ua-stack/stack-client/src/main/java/org/eclipse/milo/opcua/stack/client/transport/uasc/fsm/events/ChannelInactive.java
+++ opc-ua-stack/stack-client/src/main/java/org/eclipse/milo/opcua/stack/client/transport/uasc/fsm/events/ChannelInactive.java
@@ -15,5 +15,4 @@ package org.eclipse.milo.opcua.stack.client.transport.uasc.fsm.events;
 
 import org.eclipse.milo.opcua.stack.client.transport.uasc.fsm.ChannelFsm;
 
-public class ChannelInactive implements ChannelFsm.Event {
-}
+public class ChannelInactive implements ChannelFsm.Event {}
