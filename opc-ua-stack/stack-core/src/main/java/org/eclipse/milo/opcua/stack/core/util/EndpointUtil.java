ec8c65b00e35ac87a9b6b3869eb7dc2e309dddbf
Kevin Herron <kevinherron@gmail.com>
2019-02-12 18:22:58 -0800

    Refactor EndpointUtil.getPath() to use regex instead of Java's URI (#400)
    This allows it to extract paths from URLs that Java's URI class deems
    invalid like other methods do.

--- opc-ua-stack/stack-core/src/main/java/org/eclipse/milo/opcua/stack/core/util/EndpointUtil.java
+++ opc-ua-stack/stack-core/src/main/java/org/eclipse/milo/opcua/stack/core/util/EndpointUtil.java
@@ -13,19 +13,14 @@
 
 package org.eclipse.milo.opcua.stack.core.util;
 
-import java.net.URI;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import org.eclipse.milo.opcua.stack.core.types.structured.EndpointDescription;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class EndpointUtil {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(EndpointUtil.class);
 
     private static final Pattern ENDPOINT_URL_PATTERN =
         Pattern.compile("(opc.tcp|http|https|opc.http|opc.https|opc.ws|opc.wss)://([^:/]+)(:\\d+)?(/.*)?");
@@ -74,27 +69,26 @@ public class EndpointUtil {
      * Get the path component from an endpoint URL.
      * <p>
      * An empty path becomes "/" and trailing "/" are removed from non-empty paths.
-     * <p>
-     * If the URL is not valid the full URL will be returned.
      *
      * @param endpointUrl the endpoint URL.
      * @return the path component from the endpoint URL.
      */
+    @Nonnull
     public static String getPath(@Nonnull String endpointUrl) {
-        try {
-            URI uri = new URI(endpointUrl).parseServerAuthority();
+        Matcher matcher = ENDPOINT_URL_PATTERN.matcher(endpointUrl);
 
-            String path = uri.getPath();
+        if (matcher.matches()) {
+            String path = matcher.group(4);
 
             if (path == null || path.isEmpty()) {
                 path = "/";
             } else if (path.length() > 1 && path.endsWith("/")) {
                 path = path.substring(0, path.length() - 1);
             }
+
             return path;
-        } catch (Throwable e) {
-            LOGGER.warn("Endpoint URL '{}' is not a valid URI: {}", e.getMessage(), e);
-            return endpointUrl;
+        } else {
+            return "/";
         }
     }
 
