4c112fed73e5f5ae0a5cb7324450918ff60df5bf
Kevin Herron <kevinherron@gmail.com>
2019-11-29 14:41:57 -0800

    Add missing call to synchronize trusted CRL list

--- opc-ua-stack/stack-core/src/main/java/org/eclipse/milo/opcua/stack/core/security/DefaultTrustListManager.java
+++ opc-ua-stack/stack-core/src/main/java/org/eclipse/milo/opcua/stack/core/security/DefaultTrustListManager.java
@@ -154,6 +154,7 @@ public class DefaultTrustListManager implements TrustListManager, AutoCloseable 
         synchronizeIssuerCerts();
         synchronizeIssuerCrls();
         synchronizeTrustedCerts();
+        synchronizeTrustedCrls();
     }
 
     /**
