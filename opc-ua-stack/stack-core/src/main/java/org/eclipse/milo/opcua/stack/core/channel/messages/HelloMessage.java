2edc423d0206e160497841091d26c2c4ba266ac2
Kevin Herron <kevinherron@gmail.com>
2020-04-11 05:50:49 -0700

    Check endpoint URL length before decoding in HelloMessage

--- opc-ua-stack/stack-core/src/main/java/org/eclipse/milo/opcua/stack/core/channel/messages/HelloMessage.java
+++ opc-ua-stack/stack-core/src/main/java/org/eclipse/milo/opcua/stack/core/channel/messages/HelloMessage.java
@@ -15,20 +15,15 @@ import javax.annotation.Nonnull;
 import com.google.common.base.Charsets;
 import com.google.common.base.MoreObjects;
 import io.netty.buffer.ByteBuf;
-import org.eclipse.milo.opcua.stack.core.serialization.EncodingLimits;
+import org.eclipse.milo.opcua.stack.core.StatusCodes;
+import org.eclipse.milo.opcua.stack.core.UaException;
 import org.eclipse.milo.opcua.stack.core.util.annotations.UInt32Primitive;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
 public class HelloMessage {
 
-    private static final int MAX_ENDPOINT_URL_LENGTH = 4096;
-
-    private static final EncodingLimits ENCODING_LIMITS = new EncodingLimits(
-        EncodingLimits.DEFAULT_MAX_ARRAY_LENGTH,
-        MAX_ENDPOINT_URL_LENGTH,
-        EncodingLimits.DEFAULT_MAX_RECURSION_DEPTH
-    );
+    static final int MAX_ENDPOINT_URL_LENGTH = 4096;
 
     @UInt32Primitive
     private final long protocolVersion;
@@ -158,7 +153,7 @@ public class HelloMessage {
         encodeString(message.getEndpointUrl(), buffer);
     }
 
-    public static HelloMessage decode(ByteBuf buffer) {
+    public static HelloMessage decode(ByteBuf buffer) throws UaException {
         return new HelloMessage(
             buffer.readUnsignedIntLE(), /*    ProtocolVersion    */
             buffer.readUnsignedIntLE(), /*    ReceiveBufferSize  */
@@ -178,11 +173,24 @@ public class HelloMessage {
         }
     }
 
-    private static String decodeString(ByteBuf buffer) {
+    private static String decodeString(ByteBuf buffer) throws UaException {
         int length = buffer.readIntLE();
-        if (length == -1) {
-            return null;
+        if (length < 0) {
+            if (length == -1) {
+                return null;
+            } else {
+                throw new UaException(
+                    StatusCodes.Bad_DecodingError,
+                    "invalid endpoint URL length: " + length
+                );
+            }
         } else {
+            if (length > MAX_ENDPOINT_URL_LENGTH) {
+                throw new UaException(
+                    StatusCodes.Bad_EncodingLimitsExceeded,
+                    "endpoint URL length exceeds 4096: " + length
+                );
+            }
             byte[] bs = new byte[length];
             buffer.readBytes(bs);
             return new String(bs, Charsets.UTF_8);
