a0511dbe76c10f4acdf59d230e7903df3c821d3b
Kevin Herron <kevinherron@gmail.com>
2020-04-27 19:40:16 -0700

    Fix padding when ciphertext block size is larger than 256 bytes
    Padding bytes were being written incorrectly when the ciphertext block size
    was larger than 256 bytes, as is the case when doing asymmetric encryption
    with 4096-bit RSA keys. Fixed the padding generation, added padding
    validation to ChunkDecoder, and added ChunkSerialization tests that use
    4096-bit keys.
    
    fixes #632

--- opc-ua-stack/stack-tests/src/test/java/org/eclipse/milo/opcua/stack/SecurityFixture.java
+++ opc-ua-stack/stack-tests/src/test/java/org/eclipse/milo/opcua/stack/SecurityFixture.java
@@ -25,15 +25,25 @@ public abstract class SecurityFixture {
 
     private static final String CLIENT_ALIAS = "client-test-certificate";
     private static final String SERVER_ALIAS = "server-test-certificate";
+    private static final String CLIENT_ALIAS_4096 = "client-4096";
+    private static final String SERVER_ALIAS_4096 = "server-4096";
     private static final char[] PASSWORD = "test".toCharArray();
 
     protected volatile X509Certificate clientCertificate;
     protected volatile byte[] clientCertificateBytes;
     protected volatile KeyPair clientKeyPair;
 
+    protected volatile X509Certificate clientCertificate4096;
+    protected volatile byte[] clientCertificateBytes4096;
+    protected volatile KeyPair clientKeyPair4096;
+
     protected volatile X509Certificate serverCertificate;
     protected volatile byte[] serverCertificateBytes;
     protected volatile KeyPair serverKeyPair;
+
+    protected volatile X509Certificate serverCertificate4096;
+    protected volatile byte[] serverCertificateBytes4096;
+    protected volatile KeyPair serverKeyPair4096;
 
     protected volatile CertificateManager serverCertificateManager;
     protected volatile ServerCertificateValidator serverCertificateValidator;
@@ -44,22 +54,48 @@ public abstract class SecurityFixture {
 
         keyStore.load(getClass().getClassLoader().getResourceAsStream("test-keystore.pfx"), PASSWORD);
 
-        Key clientPrivateKey = keyStore.getKey(CLIENT_ALIAS, PASSWORD);
-        if (clientPrivateKey instanceof PrivateKey) {
-            clientCertificate = (X509Certificate) keyStore.getCertificate(CLIENT_ALIAS);
-            clientCertificateBytes = clientCertificate.getEncoded();
+        {
+            Key clientPrivateKey = keyStore.getKey(CLIENT_ALIAS, PASSWORD);
+            if (clientPrivateKey instanceof PrivateKey) {
+                clientCertificate = (X509Certificate) keyStore.getCertificate(CLIENT_ALIAS);
+                clientCertificateBytes = clientCertificate.getEncoded();
 
-            PublicKey clientPublicKey = clientCertificate.getPublicKey();
-            clientKeyPair = new KeyPair(clientPublicKey, (PrivateKey) clientPrivateKey);
+                PublicKey clientPublicKey = clientCertificate.getPublicKey();
+                clientKeyPair = new KeyPair(clientPublicKey, (PrivateKey) clientPrivateKey);
+            }
         }
 
-        Key serverPrivateKey = keyStore.getKey(SERVER_ALIAS, PASSWORD);
-        if (serverPrivateKey instanceof PrivateKey) {
-            serverCertificate = (X509Certificate) keyStore.getCertificate(SERVER_ALIAS);
-            serverCertificateBytes = serverCertificate.getEncoded();
+        {
+            Key clientPrivateKey4096 = keyStore.getKey(CLIENT_ALIAS_4096, PASSWORD);
+            if (clientPrivateKey4096 instanceof PrivateKey) {
+                clientCertificate4096 = (X509Certificate) keyStore.getCertificate(CLIENT_ALIAS_4096);
+                clientCertificateBytes4096 = clientCertificate4096.getEncoded();
 
-            PublicKey serverPublicKey = serverCertificate.getPublicKey();
-            serverKeyPair = new KeyPair(serverPublicKey, (PrivateKey) serverPrivateKey);
+                PublicKey clientPublicKey = clientCertificate4096.getPublicKey();
+                clientKeyPair4096 = new KeyPair(clientPublicKey, (PrivateKey) clientPrivateKey4096);
+            }
+        }
+
+        {
+            Key serverPrivateKey = keyStore.getKey(SERVER_ALIAS, PASSWORD);
+            if (serverPrivateKey instanceof PrivateKey) {
+                serverCertificate = (X509Certificate) keyStore.getCertificate(SERVER_ALIAS);
+                serverCertificateBytes = serverCertificate.getEncoded();
+
+                PublicKey serverPublicKey = serverCertificate.getPublicKey();
+                serverKeyPair = new KeyPair(serverPublicKey, (PrivateKey) serverPrivateKey);
+            }
+        }
+
+        {
+            Key serverPrivateKey4096 = keyStore.getKey(SERVER_ALIAS_4096, PASSWORD);
+            if (serverPrivateKey4096 instanceof PrivateKey) {
+                serverCertificate4096 = (X509Certificate) keyStore.getCertificate(SERVER_ALIAS_4096);
+                serverCertificateBytes4096 = serverCertificate4096.getEncoded();
+
+                PublicKey serverPublicKey = serverCertificate4096.getPublicKey();
+                serverKeyPair4096 = new KeyPair(serverPublicKey, (PrivateKey) serverPrivateKey4096);
+            }
         }
 
         serverCertificateManager = new TestCertificateManager(
