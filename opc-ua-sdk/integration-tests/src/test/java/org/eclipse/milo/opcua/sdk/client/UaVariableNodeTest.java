ca8a0aa235f350ee5a32528f9ea63d18095d581f
Kevin Herron <kevinherron@gmail.com>
2020-09-16 05:24:14 -0700

    Use getAttributeOrNull() on optional attributes when creating nodes
    This method will first check StatusCode::isGood, if there was a StatusCode,
    and also handles errors casting the attribute value in case a defect in the
    server results in the wrong type of value being sent.

--- opc-ua-sdk/integration-tests/src/test/java/org/eclipse/milo/opcua/sdk/client/UaVariableNodeTest.java
+++ opc-ua-sdk/integration-tests/src/test/java/org/eclipse/milo/opcua/sdk/client/UaVariableNodeTest.java
@@ -12,13 +12,23 @@ package org.eclipse.milo.opcua.sdk.client;
 
 import org.eclipse.milo.opcua.sdk.client.nodes.UaVariableNode;
 import org.eclipse.milo.opcua.sdk.client.nodes.UaVariableTypeNode;
+import org.eclipse.milo.opcua.sdk.core.AccessLevel;
+import org.eclipse.milo.opcua.sdk.core.Reference;
+import org.eclipse.milo.opcua.sdk.server.nodes.filters.AttributeFilter;
+import org.eclipse.milo.opcua.sdk.server.nodes.filters.AttributeFilterContext;
 import org.eclipse.milo.opcua.sdk.test.AbstractClientServerTest;
+import org.eclipse.milo.opcua.stack.core.AttributeId;
 import org.eclipse.milo.opcua.stack.core.Identifiers;
 import org.eclipse.milo.opcua.stack.core.UaException;
+import org.eclipse.milo.opcua.stack.core.types.builtin.LocalizedText;
+import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
+import org.eclipse.milo.opcua.stack.core.types.builtin.QualifiedName;
 import org.junit.jupiter.api.Test;
 
+import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.uint;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class UaVariableNodeTest extends AbstractClientServerTest {
 
@@ -36,6 +46,49 @@ public class UaVariableNodeTest extends AbstractClientServerTest {
         UaVariableTypeNode variableTypeNode = variableNode.getTypeDefinition();
 
         assertEquals(Identifiers.ServerStatusType, variableTypeNode.getNodeId());
+    }
+
+    @Test
+    public void readAttributeWithIncorrectDataType() throws UaException {
+        testNamespace.configureNode((nodeContext, nodeManager) -> {
+            org.eclipse.milo.opcua.sdk.server.nodes.UaVariableNode serverNode =
+                new org.eclipse.milo.opcua.sdk.server.nodes.UaVariableNode.UaVariableNodeBuilder(nodeContext)
+                    .setNodeId(new NodeId(testNamespace.getNamespaceIndex(), "IncorrectMinimumSamplingIntervalDataType"))
+                    .setAccessLevel(AccessLevel.READ_WRITE)
+                    .setUserAccessLevel(AccessLevel.READ_WRITE)
+                    .setBrowseName(new QualifiedName(testNamespace.getNamespaceIndex(), "IncorrectMinimumSamplingIntervalDataType"))
+                    .setDisplayName(LocalizedText.english("IncorrectMinimumSamplingIntervalDataType"))
+                    .setDataType(Identifiers.Double)
+                    .setTypeDefinition(Identifiers.BaseDataVariableType)
+                    .build();
+
+            serverNode.getFilterChain().addLast(new AttributeFilter() {
+                @Override
+                public Object getAttribute(AttributeFilterContext.GetAttributeContext ctx, AttributeId attributeId) {
+                    if (attributeId == AttributeId.MinimumSamplingInterval) {
+                        // intentionally return the wrong datatype
+                        return uint(100);
+                    } else {
+                        return ctx.getAttribute(attributeId);
+                    }
+                }
+            });
+
+            serverNode.addReference(new Reference(
+                serverNode.getNodeId(),
+                Identifiers.HasComponent,
+                Identifiers.ObjectsFolder.expanded(),
+                Reference.Direction.INVERSE
+            ));
+
+            nodeManager.addNode(serverNode);
+        });
+
+        UaVariableNode variableNode = client.getAddressSpace().getVariableNode(
+            new NodeId(2, "IncorrectMinimumSamplingIntervalDataType")
+        );
+
+        assertNull(variableNode.getMinimumSamplingInterval());
     }
 
 }
