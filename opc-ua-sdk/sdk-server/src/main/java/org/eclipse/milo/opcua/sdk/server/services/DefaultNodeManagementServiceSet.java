31287f17cd1eef045b61e1969daea45a3878c72c
Kevin Herron <kevinherron@gmail.com>
2020-10-06 18:17:49 -0700

    ~ implement DefaultNodeManagementServiceSet::onAddReferences and DefaultNodeManagementServiceSet::onDeleteReferences

--- opc-ua-sdk/sdk-server/src/main/java/org/eclipse/milo/opcua/sdk/server/services/DefaultNodeManagementServiceSet.java
+++ opc-ua-sdk/sdk-server/src/main/java/org/eclipse/milo/opcua/sdk/server/services/DefaultNodeManagementServiceSet.java
@@ -16,23 +16,29 @@ import org.eclipse.milo.opcua.sdk.server.DiagnosticsContext;
 import org.eclipse.milo.opcua.sdk.server.OpcUaServer;
 import org.eclipse.milo.opcua.sdk.server.Session;
 import org.eclipse.milo.opcua.sdk.server.api.services.NodeManagementServices.AddNodesContext;
+import org.eclipse.milo.opcua.sdk.server.api.services.NodeManagementServices.AddReferencesContext;
 import org.eclipse.milo.opcua.sdk.server.api.services.NodeManagementServices.DeleteNodesContext;
+import org.eclipse.milo.opcua.sdk.server.api.services.NodeManagementServices.DeleteReferencesContext;
 import org.eclipse.milo.opcua.stack.core.StatusCodes;
-import org.eclipse.milo.opcua.stack.core.UaException;
 import org.eclipse.milo.opcua.stack.core.types.builtin.DiagnosticInfo;
 import org.eclipse.milo.opcua.stack.core.types.builtin.StatusCode;
 import org.eclipse.milo.opcua.stack.core.types.structured.AddNodesItem;
 import org.eclipse.milo.opcua.stack.core.types.structured.AddNodesRequest;
 import org.eclipse.milo.opcua.stack.core.types.structured.AddNodesResponse;
 import org.eclipse.milo.opcua.stack.core.types.structured.AddNodesResult;
+import org.eclipse.milo.opcua.stack.core.types.structured.AddReferencesItem;
+import org.eclipse.milo.opcua.stack.core.types.structured.AddReferencesRequest;
+import org.eclipse.milo.opcua.stack.core.types.structured.AddReferencesResponse;
 import org.eclipse.milo.opcua.stack.core.types.structured.DeleteNodesItem;
 import org.eclipse.milo.opcua.stack.core.types.structured.DeleteNodesRequest;
 import org.eclipse.milo.opcua.stack.core.types.structured.DeleteNodesResponse;
+import org.eclipse.milo.opcua.stack.core.types.structured.DeleteReferencesItem;
+import org.eclipse.milo.opcua.stack.core.types.structured.DeleteReferencesRequest;
+import org.eclipse.milo.opcua.stack.core.types.structured.DeleteReferencesResponse;
 import org.eclipse.milo.opcua.stack.core.types.structured.ResponseHeader;
 import org.eclipse.milo.opcua.stack.server.services.NodeManagementServiceSet;
 import org.eclipse.milo.opcua.stack.server.services.ServiceRequest;
 
-import static org.eclipse.milo.opcua.stack.core.StatusCodes.Bad_ServiceUnsupported;
 import static org.eclipse.milo.opcua.stack.core.util.ConversionUtil.a;
 import static org.eclipse.milo.opcua.stack.core.util.ConversionUtil.l;
 
@@ -86,7 +92,7 @@ public class DefaultNodeManagementServiceSet implements NodeManagementServiceSet
     }
 
     @Override
-    public void onDeleteNodes(ServiceRequest service) throws UaException {
+    public void onDeleteNodes(ServiceRequest service) {
         deleteNodesMetric.record(service);
 
         OpcUaServer server = service.attr(ServiceAttributes.SERVER_KEY).get();
@@ -128,17 +134,87 @@ public class DefaultNodeManagementServiceSet implements NodeManagementServiceSet
     }
 
     @Override
-    public void onAddReferences(ServiceRequest service) throws UaException {
+    public void onAddReferences(ServiceRequest service) {
         addReferencesMetric.record(service);
 
-        service.setServiceFault(Bad_ServiceUnsupported);
+        OpcUaServer server = service.attr(ServiceAttributes.SERVER_KEY).get();
+        Session session = service.attr(ServiceAttributes.SESSION_KEY).get();
+
+        AddReferencesRequest request = (AddReferencesRequest) service.getRequest();
+
+        List<AddReferencesItem> referencesToAdd = l(request.getReferencesToAdd());
+
+        if (referencesToAdd.isEmpty()) {
+            service.setServiceFault(StatusCodes.Bad_NothingToDo);
+            return;
+        }
+
+        if (referencesToAdd.size() > server.getConfig().getLimits().getMaxNodesPerNodeManagement().longValue()) {
+            service.setServiceFault(StatusCodes.Bad_TooManyOperations);
+            return;
+        }
+
+        AddReferencesContext context = new AddReferencesContext(
+            server,
+            session,
+            new DiagnosticsContext<>()
+        );
+
+        server.getAddressSpaceManager().addReferences(context, referencesToAdd);
+
+        context.getFuture().thenAccept(results -> {
+            ResponseHeader header = service.createResponseHeader();
+
+            AddReferencesResponse response = new AddReferencesResponse(
+                header,
+                a(results, StatusCode.class),
+                new DiagnosticInfo[0]
+            );
+
+            service.setResponse(response);
+        });
     }
 
     @Override
-    public void onDeleteReferences(ServiceRequest service) throws UaException {
+    public void onDeleteReferences(ServiceRequest service) {
         deleteReferencesMetric.record(service);
 
-        service.setServiceFault(Bad_ServiceUnsupported);
+        OpcUaServer server = service.attr(ServiceAttributes.SERVER_KEY).get();
+        Session session = service.attr(ServiceAttributes.SESSION_KEY).get();
+
+        DeleteReferencesRequest request = (DeleteReferencesRequest) service.getRequest();
+
+        List<DeleteReferencesItem> referencesToDelete = l(request.getReferencesToDelete());
+
+        if (referencesToDelete.isEmpty()) {
+            service.setServiceFault(StatusCodes.Bad_NothingToDo);
+            return;
+        }
+
+        if (referencesToDelete.size() > server.getConfig().getLimits().getMaxNodesPerNodeManagement().longValue()) {
+            service.setServiceFault(StatusCodes.Bad_TooManyOperations);
+            return;
+        }
+
+        DeleteReferencesContext context = new DeleteReferencesContext(
+            server,
+            session,
+            new DiagnosticsContext<>()
+        );
+
+        server.getAddressSpaceManager().deleteReferences(context, referencesToDelete);
+
+        context.getFuture().thenAccept(results -> {
+            ResponseHeader header = service.createResponseHeader();
+
+            DeleteReferencesResponse response = new DeleteReferencesResponse(
+                header,
+                a(results, StatusCode.class),
+                new DiagnosticInfo[0]
+            );
+
+            service.setResponse(response);
+        });
     }
 
 }
