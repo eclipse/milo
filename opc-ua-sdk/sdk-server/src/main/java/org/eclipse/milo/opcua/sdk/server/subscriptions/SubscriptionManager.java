22656385464099a8a4fee15b3306137b93a48c2c
Kevin Herron <kevinherron@gmail.com>
2021-06-06 17:44:11 -0700

    Return Bad_SessionClosed for remaining PublishRequests (#857)
    When the Session is closed and the delete subscriptions flag is set de-queue
    any remaining PublishRequests and return a ServiceFault with Bad_SessionClosed.
    
    fixes #846

--- opc-ua-sdk/sdk-server/src/main/java/org/eclipse/milo/opcua/sdk/server/subscriptions/SubscriptionManager.java
+++ opc-ua-sdk/sdk-server/src/main/java/org/eclipse/milo/opcua/sdk/server/subscriptions/SubscriptionManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 the Eclipse Milo Authors
+ * Copyright (c) 2021 the Eclipse Milo Authors
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -1221,6 +1221,15 @@ public class SubscriptionManager {
             }
 
             iterator.remove();
+        }
+
+        if (deleteSubscriptions) {
+            while (publishQueue.isNotEmpty()) {
+                ServiceRequest publishService = publishQueue.poll();
+                if (publishService != null) {
+                    publishService.setServiceFault(StatusCodes.Bad_SessionClosed);
+                }
+            }
         }
     }
 
