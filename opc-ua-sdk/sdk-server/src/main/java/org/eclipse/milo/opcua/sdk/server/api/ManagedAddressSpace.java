c889a2b9ee741be422a77c9e9b7e83650c64f46b
Kevin Herron <kevinherron@gmail.com>
2022-11-05 06:31:08 -0700

    Allow public access to some ManagedAddressSpace fields

--- opc-ua-sdk/sdk-server/src/main/java/org/eclipse/milo/opcua/sdk/server/api/ManagedAddressSpace.java
+++ opc-ua-sdk/sdk-server/src/main/java/org/eclipse/milo/opcua/sdk/server/api/ManagedAddressSpace.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 the Eclipse Milo Authors
+ * Copyright (c) 2022 the Eclipse Milo Authors
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -87,15 +87,15 @@ public abstract class ManagedAddressSpace implements AddressSpace {
         return server;
     }
 
-    protected UaNodeContext getNodeContext() {
+    public UaNodeContext getNodeContext() {
         return nodeContext;
     }
 
-    protected NodeFactory getNodeFactory() {
+    public NodeFactory getNodeFactory() {
         return nodeFactory;
     }
 
-    protected UaNodeManager getNodeManager() {
+    public UaNodeManager getNodeManager() {
         return nodeManager;
     }
 
