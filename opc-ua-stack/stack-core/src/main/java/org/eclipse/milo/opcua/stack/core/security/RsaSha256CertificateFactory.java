2ef75835ed7c831c5abbb8448cd2ae40739ef327
Kevin Herron <kevinherron@gmail.com>
2023-09-05 19:23:53 -0700

    ~ javadocs, formatting

--- opc-ua-stack/stack-core/src/main/java/org/eclipse/milo/opcua/stack/core/security/RsaSha256CertificateFactory.java
+++ opc-ua-stack/stack-core/src/main/java/org/eclipse/milo/opcua/stack/core/security/RsaSha256CertificateFactory.java
@@ -46,7 +46,15 @@ public abstract class RsaSha256CertificateFactory implements CertificateFactory 
     }
 
     @Override
-    public ByteString createSigningRequest(NodeId certificateTypeId, KeyPair keyPair, X500Name subjectName, String sanUri, List<String> dnsNames, List<String> ipAddresses) throws Exception {
+    public ByteString createSigningRequest(
+        NodeId certificateTypeId,
+        KeyPair keyPair,
+        X500Name subjectName,
+        String sanUri,
+        List<String> dnsNames,
+        List<String> ipAddresses
+    ) throws Exception {
+
         if (NodeIds.RsaSha256ApplicationCertificateType.equals(certificateTypeId)) {
             return createRsaSha256SigningRequest(keyPair, subjectName, sanUri, dnsNames, ipAddresses);
         } else {
@@ -54,12 +62,39 @@ public abstract class RsaSha256CertificateFactory implements CertificateFactory 
         }
     }
 
+    /**
+     * Create a new {@link KeyPair} for a {@link NodeIds#RsaSha256ApplicationCertificateType}
+     * type certificate. The key length must be between 2048 and 4096 bits.
+     *
+     * @return the new {@link KeyPair}.
+     */
     protected KeyPair createRsaSha256KeyPair() throws NoSuchAlgorithmException {
         return SelfSignedCertificateGenerator.generateRsaKeyPair(DEFAULT_KEY_LENGTH);
     }
 
+    /**
+     * Create a new {@link X509Certificate} chain of type
+     * {@link NodeIds#RsaSha256ApplicationCertificateType}. The chain may be of length 1 if the
+     * certificate is self-signed.
+     *
+     * @param keyPair the {@link KeyPair} to use when creating the certificate chain.
+     * @return the new {@link X509Certificate} chain.
+     * @throws Exception if an error occurs while creating the certificate chain.
+     */
     protected abstract X509Certificate[] createRsaSha256CertificateChain(KeyPair keyPair) throws Exception;
 
+    /**
+     * Create a new PKCS10 certificate signing request for a
+     * {@link NodeIds#RsaSha256ApplicationCertificateType} type certificate.
+     *
+     * @param keyPair the {@link KeyPair} to use when creating the signing request.
+     * @param subjectName the {@link X500Name} to request.
+     * @param sanUri the URI to request in the Subject Alternative Name of the CSR.
+     * @param dnsNames the DNS names to request in the Subject Alternative Name of the CSR.
+     * @param ipAddresses the IP addresses to request in the Subject Alternative Name of the CSR.
+     * @return the new {@link ByteString} containing the DER-encoded PKCS10 signing request.
+     * @throws Exception if an error occurs while creating the signing request.
+     */
     protected ByteString createRsaSha256SigningRequest(
         KeyPair keyPair,
         X500Name subjectName,
