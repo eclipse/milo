2a5801000ab717a56709ea4793349bd23fa51880
Kevin Herron <kevinherron@gmail.com>
2023-10-13 12:21:42 +0200

    Generate certificates with BasicConstraints cA=false
    Setting cA=false is the recommended behavior for self-signed certificates in OPC UA 1.05, but implementations are supposed to continue to accept certificates with cA=true as long as the path length is explicitly set to 0 (and not undefined).

--- opc-ua-stack/stack-core/src/main/java/org/eclipse/milo/opcua/stack/core/util/SelfSignedCertificateGenerator.java
+++ opc-ua-stack/stack-core/src/main/java/org/eclipse/milo/opcua/stack/core/util/SelfSignedCertificateGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 the Eclipse Milo Authors
+ * Copyright (c) 2023 the Eclipse Milo Authors
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -129,7 +129,7 @@ public class SelfSignedCertificateGenerator {
         );
 
         // Explicitly set path length constraint to 0. This constructor also sets cA=true.
-        BasicConstraints basicConstraints = new BasicConstraints(0);
+        BasicConstraints basicConstraints = new BasicConstraints(false);
 
         // Authority Key Identifier
         addAuthorityKeyIdentifier(certificateBuilder, keyPair);
