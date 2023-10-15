69d5a4b23928c3f407d39c3bdf138971e8419e91
Kevin Herron <kevinherron@gmail.com>
2023-10-15 06:40:44 -0700

    WIP RoleManager API

--- opc-ua-sdk/sdk-server/src/main/java/org/eclipse/milo/opcua/sdk/server/RoleManager.java
+++ opc-ua-sdk/sdk-server/src/main/java/org/eclipse/milo/opcua/sdk/server/RoleManager.java
@@ -1,12 +1,27 @@
+/*
+ * Copyright (c) 2023 the Eclipse Milo Authors
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
 package org.eclipse.milo.opcua.sdk.server;
 
 import java.util.List;
 
 import org.eclipse.milo.opcua.sdk.server.identity.Identity;
 import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
+import org.eclipse.milo.opcua.stack.core.types.structured.EndpointDescription;
 
 public interface RoleManager {
 
     List<NodeId> getRoleIds(Identity identity);
+
+    default List<NodeId> getRoleIds(Identity identity, String applicationUri, EndpointDescription endpoint) {
+        return getRoleIds(identity);
+    }
 
 }
