52e97dec76ef47d9a5a1e55a83b682c073f0808d
Kevin Herron <kevinherron@gmail.com>
2024-01-31 14:47:01 -0800

    ~ WIP

---
+++ opc-ua-sdk/integration-tests/src/test/java/org/eclipse/milo/opcua/sdk/client/subscriptions2/OpcUaMonitoredItemTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2024 the Eclipse Milo Authors
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+package org.eclipse.milo.opcua.sdk.client.subscriptions2;
+
+import org.eclipse.milo.opcua.sdk.test.AbstractClientServerTest;
+import org.eclipse.milo.opcua.stack.core.NodeIds;
+import org.eclipse.milo.opcua.stack.core.UaException;
+import org.eclipse.milo.opcua.stack.core.types.builtin.StatusCode;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OpcUaMonitoredItemTest extends AbstractClientServerTest {
+
+    private final OpcUaSubscription subscription = new OpcUaSubscription(client);
+
+    @AfterEach
+    void tearDown() throws UaException {
+        subscription.delete();
+    }
+
+    @Test
+    void createMonitoredItem() throws UaException {
+        var monitoredItem = OpcUaMonitoredItem.newDataItem(
+            subscription,
+            NodeIds.Server_ServerStatus_CurrentTime
+        );
+
+        try {
+            StatusCode result = monitoredItem.create();
+
+            assertEquals(StatusCode.GOOD, result);
+        } finally {
+            monitoredItem.delete();
+        }
+    }
+
+}
