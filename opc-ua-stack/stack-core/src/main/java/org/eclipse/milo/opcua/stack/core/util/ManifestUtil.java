cd0f3d97f78de3823541229032642f8af9f00040
Kevin Herron <kevinherron@gmail.com>
2024-02-09 04:45:02 -0800

    Refactor ManifestUtil, remove conversion to URI (#1218)
    fixes #1216

--- opc-ua-stack/stack-core/src/main/java/org/eclipse/milo/opcua/stack/core/util/ManifestUtil.java
+++ opc-ua-stack/stack-core/src/main/java/org/eclipse/milo/opcua/stack/core/util/ManifestUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 the Eclipse Milo Authors
+ * Copyright (c) 2024 the Eclipse Milo Authors
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -10,16 +10,12 @@
 
 package org.eclipse.milo.opcua.stack.core.util;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.util.Collections;
 import java.util.Enumeration;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
@@ -30,38 +26,40 @@ import org.slf4j.LoggerFactory;
 public class ManifestUtil {
 
     private static final Logger logger = LoggerFactory.getLogger(ManifestUtil.class);
-    private static final Map<String, String> attributes = load();
+    private static final Map<String, String> ATTRIBUTES = readAttributes();
 
     public static Optional<String> read(String name) {
-        return Optional.ofNullable(attributes.get(name));
+        return Optional.ofNullable(ATTRIBUTES.get(name));
     }
 
     public static boolean exists(String name) {
-        return attributes.containsKey(name);
+        return ATTRIBUTES.containsKey(name);
     }
 
-    private static Map<String, String> load() {
+    private static Map<String, String> readAttributes() {
         Map<String, String> attributes = Maps.newConcurrentMap();
 
-        for (URI uri : uris()) {
-            try {
-                attributes.putAll(load(uri.toURL()));
-            } catch (Throwable t) {
-                logger.error("load(): '{}' failed", uri, t);
+        try {
+            Enumeration<URL> resources = ManifestUtil.class
+                .getClassLoader()
+                .getResources("META-INF/MANIFEST.MF");
+
+            while (resources.hasMoreElements()) {
+                URL url = resources.nextElement();
+
+                attributes.putAll(readAttributes(url));
             }
+        } catch (Throwable t) {
+            logger.error("readAttributes() failed", t);
         }
 
         return attributes;
     }
 
-    private static Map<String, String> load(URL url) throws IOException {
-        return load(url.openStream());
-    }
+    private static Map<String, String> readAttributes(URL url) {
+        Map<String, String> props = new HashMap<>();
 
-    private static Map<String, String> load(InputStream stream) {
-        Map<String, String> props = Maps.newConcurrentMap();
-
-        try {
+        try (InputStream stream = url.openStream()) {
             Manifest manifest = new Manifest(stream);
             Attributes attributes = manifest.getMainAttributes();
 
@@ -70,34 +68,10 @@ public class ManifestUtil {
                 props.put(key.toString(), value);
             }
         } catch (Throwable t) {
-            logger.error("#load(): failed", t);
-        } finally {
-            try {
-                stream.close();
-            } catch (IOException e) {
-                // ignored
-            }
+            logger.error("readAttributes(): '{}' failed", url, t);
         }
 
         return props;
-    }
-
-    private static Set<URI> uris() {
-        try {
-            Enumeration<URL> resources = ManifestUtil.class
-                .getClassLoader()
-                .getResources("META-INF/MANIFEST.MF");
-
-            Set<URI> uris = new HashSet<>();
-            while (resources.hasMoreElements()) {
-                uris.add(resources.nextElement().toURI());
-            }
-
-            return uris;
-        } catch (Throwable t) {
-            logger.error("uris() failed", t);
-            return Collections.emptySet();
-        }
     }
 
 }
