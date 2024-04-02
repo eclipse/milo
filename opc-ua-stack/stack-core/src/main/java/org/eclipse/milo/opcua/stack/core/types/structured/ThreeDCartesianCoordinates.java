7bcfcc20d9ad44a478f765ece741a5291aff96ce
Kevin Herron <kevinherron@gmail.com>
2024-04-02 08:38:15 -0700

    Run Optimize Imports on generated structures

--- opc-ua-stack/stack-core/src/main/java/org/eclipse/milo/opcua/stack/core/types/structured/ThreeDCartesianCoordinates.java
+++ opc-ua-stack/stack-core/src/main/java/org/eclipse/milo/opcua/stack/core/types/structured/ThreeDCartesianCoordinates.java
@@ -10,11 +10,6 @@
 
 package org.eclipse.milo.opcua.stack.core.types.structured;
 
-import java.lang.Class;
-import java.lang.Double;
-import java.lang.Object;
-import java.lang.Override;
-import java.lang.String;
 import java.util.StringJoiner;
 
 import org.eclipse.milo.opcua.stack.core.NamespaceTable;
